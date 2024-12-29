fcd18b2fb94271086f117c1b2fcda15a74f8aa99
ArielHernandez4 <92188731+ArielHernandez4@users.noreply.github.com>
2024-12-28 23:32:07 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,3 +28,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor en ejecución en el puerto ${PORT}`));
+
+module.exports = app;
+
