695dc84f2c9c2d859b192a1092f449a6cdadb3a5
ArielHernandez4 <92188731+ArielHernandez4@users.noreply.github.com>
2024-12-31 01:54:48 -0600

    Update10 server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRoutes);
 
